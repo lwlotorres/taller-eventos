ccd5740a515a5a0ea47a6838fcf345c2e588d3ee
lwlotorres <lucas_torres.perez@hotmail.com>
2023-08-08 15:47:51 -0300

    Se soluciona el problema

--- eventos.js
+++ eventos.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(){
     let eventoDiv = document.getElementById("division");
+    let button = document.getElementById("boton")
 
     eventoDiv.addEventListener("click", function(){
         alert("Hola! Soy el div");
 
+    })
+    button.addEventListener("click", function(event){
+        event.stopPropagation()
     })
 })
