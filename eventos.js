9400cf6e55c2938f6639f31fd839325837efcae9
lwlotorres <lucas_torres.perez@hotmail.com>
2023-08-08 15:19:40 -0300

    Se agrega alerta en div

---
+++ eventos.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let eventoDiv = document.getElementById("division");
+
+    eventoDiv.addEventListener("click", function(){
+        alert("Hola! Soy el div");
+
+    })
+})
